543d45fde8e9ddc84eec708166d6e06aa92ee4f5
Giggioh <33316356+Giggioh@users.noreply.github.com>
2018-03-01 12:59:55 +0100

    Creazione utente prova
    basata su sails 1.0

--- web/api/controllers/UtenteController.js
+++ web/api/controllers/UtenteController.js
@@ -12,7 +12,29 @@ module.exports = {
   },
 
   create: function(req,res,next) {
+    //dati login
+    username=req.param("username");
+    password=req.param("password");
 
+    //dati utente
+    nome=req.param("nome");
+    cognome=req.param("cognome");
+
+    sails..transaction(function(db,proceed) {
+      Account.create({username: username, password: password}).exec(function(err,account) {
+        if (err) return proceed(err);
+
+        Utente.create({nome: nome, cognome: cognome, account: account}).exec(function(err,utente) {
+          if (err) return proceed(err);
+
+          return proceed();
+        });
+      });
+    }).exec(function(err) {
+      if (err) next(err);
+
+      res.redirect("/utente");
+    });
   }
 
 };
