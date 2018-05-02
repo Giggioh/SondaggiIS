347c98c30ee18c59bfa02c1cd0d2bcedf925676e
itahailstorm <33965863+itahailstorm@users.noreply.github.com>
2018-05-02 15:50:56 +0200

    homepage
    homepage fatta e funzionante

--- web/config/routes.js
+++ web/config/routes.js
@@ -33,7 +33,7 @@ module.exports.routes = {
   ***************************************************************************/
 
   '/': {
-    view: 'homepage'
+    view: 'Home/index'
   },
 
   // AccountController
